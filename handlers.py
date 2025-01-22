f4f0be6ec13b881ed8b59a6feda0703851a8ac43
Maria Mikh <hamaha2013@gmail.com>
2025-01-23 01:51:33 +0300

    Telegram bot v.2 - new functions deployed

--- handlers.py
+++ handlers.py
@@ -7,6 +7,7 @@ from utils import get_temperature, get_food_info
 #import aiohttp
 
 router = Router()
+user = {}
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
@@ -51,38 +52,42 @@ async def process_city(message: Message, state: FSMContext):
 
 @router.message(Form.callories)
 async def process_callories(message: Message, state: FSMContext):
+    global user
     await state.update_data(callories=message.text)
     #await message.reply(f"Проверьте данные {d.get('weight')} {d.get('height')} \n")
     data = await state.get_data()
-    print(f"DEBUG: Сохраненные данные: {data}")
+    user = data
+    print(f"DEBUG: Сохраненные данные: {user}")
     await message.reply("Спасибо за заполнение анкеты! \n"
                         "Чтобы увидеть норму воды и каллорий введите /calculate")
+    await state.clear()
 
 #Функция по дальнейшим шагам: рассчитать норму
 @router.message(Command("calculate"))
-async def calculate(message: Message, state: FSMContext):
-    data = await state.get_data()
-
-    print(f"DEBUG: Данные для расчета: {data}")
-    #get_temperature
-    #temp = get_temperature(str(data.get('city')))
-    #calculate water norm
-    if data:
-        city = data.get('city')
-        weight = data.get('weight')
-        height = data.get('height')
-        callories = data.get('callories')
-
-        # Пример вывода информации
-        response_message = (f"Город: {city}\n"
-                            f"Вес: {weight}\n"
-                            f"Рост: {height}\n"
-                            f"Калории: {callories}")
-
-        await message.reply(response_message)
+async def calculate(message: Message):
+    global user
+    print(f"DEBUG: Данные для расчета: {user}")
+    city = user['city']
+    weight = float(user['weight'])
+    height = float(user['height'])
+    active = int(user['activity'])
+    callories_aim = float(user['callories'])
+    age = int(user['age'])
+    # get_temperature
+    temp = float(get_temperature(city))
+    print(f"DEBUG: Данные по подключению к Weather API - результат температура: {temp}")
+    # calculate water norm
+    water = float(weight) * 30 + 500 * int(active) / 30 + 500 * temp / 25
+    user['water_norm'] = water
+    # check callories norm
+    if callories_aim is None:
+        callories_aim = 10 * weight + 6.25 * height - 5 * age + 200 * active / 30
+        user['cal_norm'] = callories_aim
     else:
-        # Сообщение на случай, если данные недоступны
-        await message.reply("Данные не найдены. Пожалуйста, заполните анкету заново.")
+        user['cal_norm'] = callories_aim
+    await message.answer(f"Норма воды: {water} \n"
+                        f"Норма (или цель) каллорий: {callories_aim}\n"
+                        f"Для внесения информации по потреблению воды, каллорий и пройденным активностям, введите /help")  #: {rate:.5f}")
 
 
 @router.message(Command("help"))
@@ -94,13 +99,13 @@ async def cmd_help(message: Message):
         "/log_workout <тип тренировки> <время (мин)> - Фиксирует сожжённые калории.\n"
         "/check_progress - Прогресс по воде и калориям")
 
-
+#логгируем воду
 @router.message(Command("log_water"))
 async def cmd_water(
         message: Message,
-        command: CommandObject,
-        state: FSMContext
+        command: CommandObject
 ):
+    global user
     # Если не переданы никакие аргументы, то
     # command.args будет None
     if command.args is None:
@@ -108,19 +113,82 @@ async def cmd_water(
             "Ошибка: не переданы аргументы"
         )
         return
-    data = await state.get_data()
-    water_norm = int(data['water_norm'])
-    if data['water_log'] is None:
-        await state.update_data(water_log=int(command.args))
     else:
-        water_new = int(data['water_log']) + int(command.args)
-        await state.update_data(water_log=water_new)
+        water_log = int(command.args)
+    water_norm = int(user['water_norm'])
+    if 'water_log' not in user.keys():
+        user['water_log'] = water_log
+    else:
+        user['water_log'] += water_log
 
-    water_diff = water_norm - int(data['water_log'])
+    water_diff = water_norm - user['water_log']
     if water_diff >0:
         await message.answer(f"До выполнения нормы осталось {water_diff} мл")
     else:
         await message.answer(f"Норма выполнена!")
+
+#логгируем каллории
+@router.message(Command("log_food"))
+async def cmd_food(
+        message: Message,
+        command: CommandObject
+):
+    global user
+    # Если не переданы никакие аргументы, то
+    # command.args будет None
+    if command.args is None:
+        await message.answer(
+            "Ошибка: не переданы аргументы"
+        )
+        return
+    else:
+        food = command.args
+    callory = get_food_info(food)
+    print(f"DEBUG: Данные по подключению к Food API - result: {callory}")
+    user['callory'] = float(callory['calories'])
+    await message.answer(f"{food} - {user['callory']} каллорий. Сколько грамм вы съели?")
+
+@router.message()
+async def log_apifood(message: Message):
+    global user
+    gram = float(message.text)
+    prod_callory = gram/100 * user['callory']
+    if 'cal_log' not in user.keys():
+        user['cal_log'] = prod_callory
+    else:
+        user['cal_log'] += prod_callory
+    await message.answer(f"Записано {prod_callory} ккал")
+
+#запись тренировки
+@router.message(Command("log_workout"))
+async def cmd_workout(
+        message: Message,
+        command: CommandObject
+):
+    global user
+    # Если не переданы никакие аргументы, то
+    # command.args будет None
+    if command.args is None:
+        await message.answer(
+            "Ошибка: не переданы аргументы"
+        )
+        return
+    # Пробуем разделить аргументы на две части по первому встречному пробелу
+    try:
+        work_type, work_time = command.args.split(" ", maxsplit=1)
+    except ValueError:
+        await message.answer(
+            "Ошибка: неправильный формат команды. Пример:\n"
+            "/log_workout <тип> <время в мин>"
+        )
+
+    weight = float(user['weight'])
+    off_call = 6 * float(work_time)/60.0 *  weight
+    water_needed = 200 * float(work_time)/30.0
+    user['needed_water'] = water_needed
+    user['off_calories'] = off_call
+    await message.answer(f"{work_type} {work_time} минут - {off_call} ккал. \n"
+                         f"Дополнительно выпейте {water_needed} мл воды.")
 
 # Функция для подключения обработчиков
 def setup_handlers(dp):
